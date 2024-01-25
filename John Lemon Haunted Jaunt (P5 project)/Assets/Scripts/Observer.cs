f26ca6cee817d7d18eb126e415d3c1e1b0107a8b
889Error143 <113727616+889Error143@users.noreply.github.com>
2024-01-25 14:00:27 -0800

    finish

---
+++ John Lemon Haunted Jaunt (P5 project)/Assets/Scripts/Observer.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Observer : MonoBehaviour
+{
+    public Transform player;
+    public GameEnding gameEnding;
+
+
+    bool m_IsplayerInRange;
+
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.transform == player)
+        {
+            m_IsplayerInRange = true;
+        }
+    }
+
+    void OnTriggerExit (Collider other)
+    {
+     if (other.transform == player)
+        {
+            m_IsplayerInRange = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_IsplayerInRange)
+        {
+            Vector3 direction = player.position - transform.position + Vector3.up;
+            Ray ray = new Ray(transform.position, direction);
+            RaycastHit raycastHit;
+
+
+            if (Physics.Raycast(ray, out raycastHit))
+            {
+                if (raycastHit.collider.transform == player)
+                {
+                    gameEnding.CaughtPlayer();
+                }
+            }
+
+        }
+
+        }
+
+}
